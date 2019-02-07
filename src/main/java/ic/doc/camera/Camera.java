9eed9a362ed650e40647105d2694a5009601fb0c
Tristan Clark <tc4518@edge04.doc.ic.ac.uk>
2019-02-07 12:19:54 +0000

    Switching camera on and off implemented

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,17 +1,47 @@
 package ic.doc.camera;
 
-public class Camera {
+public class Camera implements WriteListener {
+
+  private Sensor cameraSensor;
+  private MemoryCard cameraMemoryCard;
+  private boolean power;
+  private boolean dataBeingWritten;
+
+  public Camera(Sensor cameraSensor, MemoryCard cameraMemoryCard) {
+    this.cameraSensor = cameraSensor;
+    this.cameraMemoryCard = cameraMemoryCard;
+    this.power = false;
+    this.dataBeingWritten = false;
+  }
 
   public void pressShutter() {
-    // not implemented
+    if (power) {
+      dataBeingWritten = true;
+      byte[] data = cameraSensor.readData();
+      cameraMemoryCard.write(data);
+    }
   }
 
   public void powerOn() {
-    // not implemented
+    power = true;
+    cameraSensor.powerUp();
   }
 
   public void powerOff() {
-    // not implemented
+    if (!dataBeingWritten) {
+      cameraSensor.powerDown();
+    }
+    power = false;
+  }
+
+  public boolean isOn() {
+    return power;
+  }
+
+  @Override
+  public void writeComplete() {
+    dataBeingWritten = false;
+    cameraSensor.powerDown();
   }
 }
 
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,5 +1,9 @@
 package ic.doc.camera;
 
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,10 +13,91 @@ public class CameraTest {
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
+  WriteListener cameraWriteListener = context.mock(WriteListener.class);
+  Sensor cameraSensor = context.mock(Sensor.class);
+  MemoryCard cameraMemoryCard = context.mock(MemoryCard.class);
+  Camera camera = new Camera(cameraSensor, cameraMemoryCard);
+  byte[] data = new byte[0];
+
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
 
-    // write your test here
+    context.checking(new Expectations() {{
+      exactly(1).of(cameraSensor).powerUp();
+    }});
+
+    camera.powerOn();
   }
 
+  @Test
+  public void switchingTheCameraOffPowersDownTheSensor() {
+
+    context.checking(new Expectations() {{
+      exactly(1).of(cameraSensor).powerDown();
+    }});
+
+    camera.powerOff();
+  }
+
+
+  @Test
+  public void pressingTheShutterWhenThePowerIsOffDoesNothing() {
+
+    context.checking(new Expectations() {{
+      never(cameraSensor);
+      never(cameraMemoryCard);
+    }});
+
+    assertFalse(camera.isOn());
+    camera.pressShutter();
+  }
+
+  @Test
+  public void pressingShutterWhenPowerOnCopiesDataFromSensorToMemCard() {
+
+    context.checking(new Expectations() {{
+      exactly(1).of(cameraSensor).powerUp();
+      exactly(1).of(cameraSensor).readData();
+      will(returnValue(data));
+      exactly(1).of(cameraMemoryCard).write(data);
+    }});
+
+    camera.powerOn();
+    assertTrue(camera.isOn());
+    camera.pressShutter();
+  }
+
+  @Test
+  public void ifDataIsCurrentlyBeingWrittenSwitchingOffPowerDoesNotPowerDownSensor() {
+
+    context.checking(new Expectations() {{
+      exactly(1).of(cameraSensor).powerUp();
+      exactly(1).of(cameraSensor).readData();
+      will(returnValue(data));
+      exactly(1).of(cameraMemoryCard).write(data);
+      never(cameraSensor).powerDown();
+    }});
+
+    camera.powerOn();
+    camera.pressShutter();
+    camera.powerOff();
+  }
+
+
+  @Test
+  public void onceWritingDataHasCompletedCameraPowersDownIfPressed() {
+
+    context.checking(new Expectations() {{
+      exactly(1).of(cameraSensor).powerUp();
+      exactly(1).of(cameraSensor).readData();
+      will(returnValue(data));
+      exactly(1).of(cameraMemoryCard).write(data);
+      exactly(1).of(cameraSensor).powerDown();
+    }});
+
+    camera.powerOn();
+    camera.pressShutter();
+    camera.powerOff();
+    camera.writeComplete();
+  }
 }
